42e7dd97921a4cf1fca3b6877da46daed8ebdf79
Glitch (socketio-template) <none>
2022-06-05 00:27:54 +0000

    🎹🦡 Updated with Glitch

---
+++ public/output/sketch.js
@@ -0,0 +1,17 @@
+//open and connect the input socket
+let socket = io('/output');
+
+//listen for the confirmation of connection 
+socket.on('connect', function(){
+  console.log('output connected to server');
+});
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw(){
+
+}
+
